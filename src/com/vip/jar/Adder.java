7f8a8abe5cb0def2855737bcf298df5006e39b30
deng.wu <deng.wu@vipshop.com>
2017-06-04 18:45:00 +0800

    Initial commit

---
+++ src/com/vip/jar/Adder.java
@@ -0,0 +1,10 @@
+package com.vip.jar;
+
+/**
+ * Created by wudeng on 17/6/2.
+ */
+public class Adder {
+    public int add(int i, int j) {
+        return i + j;
+    }
+}
---
+++ src/com/vip/jar/Client.java
@@ -0,0 +1,10 @@
+package com.vip.jar;
+
+/**
+ * Created by wudeng on 17/6/2.
+ */
+public class Client {
+    public static void main(String[] args){
+        System.out.println("hello client");
+    }
+}
